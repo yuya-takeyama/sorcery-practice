4bc60ec1bb593216f0e1c113e8f05be2a59c1b2f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2015-09-25 19:44:33 +0900

    Fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes['password'] }
   validates :password, confirmation: true, if: -> { new_record? || changes['password'] }
-  validates :password_confirmation, precense: true, if: -> { new_record? || changes['password'] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes['password'] }
 
   validates :email, uniqueness: true
 end
